3df31435dfa87892e7fc1869fe8d92b8af95cb4a
sunflawlxs <sherylivana99@gmail.com>
2023-10-27 20:30:29 +0700

    design login, register uda slese, sisa account aja yang belom

--- Bookphoria/models.py
+++ Bookphoria/models.py
@@ -1,22 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from Homepage.models import Book
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=255)
-    liked_book = models.ManyToManyField(Book, related_name='likers', blank=True)
+    username = models.CharField(max_length=255)
+    fullname= models.CharField(max_length=255)
+    liked_books = models.ManyToManyField(Book, related_name='likers', blank=True)
     country = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=255, blank=True)
     age = models.PositiveIntegerField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.TextField( blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True) 
+    password = models.CharField(max_length = 255, blank=False, null =False)
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields =  ['full_name', 'profile_picture', 'age','phone_number',  'country', 'city', 'liked_book', ]
+        fields =  ['username',  'fullname', 'password','profile_picture', 'age','phone_number',  'country', 'city' ]
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields =  ['username','fullname', 'password','profile_picture', 'age','phone_number',  'country', 'city']
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- Bookphoria/views.py
+++ Bookphoria/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
@@ -7,7 +7,7 @@ from Bookphoria.forms import ReviewForm
 from django.http import HttpResponse
 from django.core import serializers
 from django.urls import reverse
-from Bookphoria.models import Review, UserProfile, UserProfileForm
+from Bookphoria.models import EditProfileForm, Review, UserProfile, UserProfileForm
 from django.contrib import messages
 from django.db.models import Sum
 from django.contrib.auth import authenticate, login, logout
@@ -15,20 +15,38 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from Bookphoria.models import Review
 from Bookphoria.forms import ReviewForm
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+
+import json
+
 
 from Homepage.models import Book
 
 def register(request):
-    form = UserCreationForm()
-
+    form = UserProfileForm()
+    print("silahkan" )
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('login')
+        username= request.POST['username']
+        fullname = request.POST['fullname']
+        age =int(request.POST['age'])
+        country = request.POST['country']
+        city = request.POST['city']
+        phone_number = request.POST['phone_number']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']#validasi password
+        user = User.objects.create_user(username=username, password=password1)
+        user.save()
+        user_profile = UserProfile(user=user,fullname = fullname, username=username, age=age, country=country, city=city, phone_number=phone_number)
+        user_profile.save() 
+        print("silahkan lagi...")
+        messages.success(request, 'Your account has been successfully created!')
+        return redirect('/login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+
 
 def login_user(request):
     if request.method == 'POST':
@@ -66,13 +84,15 @@ def create_profile(request):
 @login_required
 def view_profile(request):
     user = request.user
+    userProfile = UserProfile.objects.get(user=user)
+    print(userProfile.country)
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
     else:
         form = UserProfileForm(instance=user)
-    return render(request, 'user.html', {'form': form})
+    return render(request, 'user.html', {'form': form, 'userProfile':userProfile})
 
 def review_list(request):
     reviews = Review.objects.all()
@@ -95,3 +115,32 @@ def add_review(request, product_id):
 
     return render(request, 'user.html', {'form': form})
 
+def edit_profile(request):
+    form = EditProfileForm(instance=request.user)
+    userProfile = UserProfile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('view_profile')  # Ganti 'profile' dengan URL profil pengguna
+        
+            
+    return render(request, 'edituser.html', {'form': form, 'userProfile':userProfile})
+
+@csrf_exempt
+def get_previous_edit_data_json(request):
+    data = json.loads(request.body)
+    print(data)
+    userId = data['id']
+    userProfile = UserProfile.objects.get(pk=userId)
+    userProfileData = {
+        'username': userProfile.username,
+        'password': userProfile.password,
+        'fullname': userProfile.fullname,
+        'age': userProfile.age,
+        'country': userProfile.country,
+        'city': userProfile.city,
+        'phoneNumber':userProfile.phone_number
+
+    }
+    return JsonResponse(userProfileData)
