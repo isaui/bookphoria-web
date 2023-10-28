3763ed3798d1173dba5aaf615ee442db74a718e7
CyberSleeper <mahartha.gemilang@gmail.com>
2023-10-28 16:35:51 +0700

    Moved the JS to static files

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,28 +1,51 @@
-const getUser = async (username) => {
+async function refreshBookList() {
   const loading = document.getElementById('loading-text')
   try {
-    loading.style.display = 'flex';
-    const res = await fetch(`/get-user/${username}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await res.json();
-    if (data) {
-      console.log(data);
-      document.querySelector("#username").textContent = data.username;
-      document.querySelector("#email").textContent = data.email;
-    }
-    loading.style.display = 'none';
+      loading.style.display = 'flex';
+      const bookList = document.getElementById("book-list")
+      const response = await fetch("/profile/get-books")
+      console.log("====================================")
+      console.log(response)
+      const data = await response.json()
+      console.log(data)
+      console.log("====================================")
+      bookList.innerHTML = ""
+      if (data.books.length == 0){
+          bookList.innerHTML = `<p>You have no books :(</p>`
+      } else {
+          data.books.forEach(book => {
+              bookList.innerHTML += "\n" +
+                  `<img src="${book.thumbnail}" class="h-full w-auto object-contain shadow-xl"/>`
+          })
+      }
+      loading.style.display = 'none';
   } catch (err) {
-    loading.style.display = 'none';
-    console.log(err);
+      console.log(err)
+      loading.style.display = 'none';
   }
 }
 
 window.addEventListener("DOMContentLoaded", async ()=>{
-  const params = new URLSearchParams(window.location.search);
-  const username = params.get('username');
-  console.log(username)
-  await getUser(username);
+  await refreshBookList()
+  console.log("DOM Loaded")
   homeContent.classList.remove("hidden"); 
+  function addBook() {
+      fetch("{% url 'Dashboard:add_book' %}", {
+          method: "POST",
+          body: new FormData(document.querySelector('#add-book-form'))
+      }).then(refreshBookList)
+
+      document.getElementById("add-book-form").reset()
+      document.querySelector('[data-modal-hide="add-book-modal"]').click()
+  }
+
+  document.getElementById("submit_new_book").onclick = addBook
+
+  function validateSize(input) {
+      const fileSize = input.files[0].size / 1024 / 1024; // in MiB
+      if (fileSize > 2) {
+          alert('File size exceeds 2 MiB');
+          input.value = '';
+      }
+  }
 })
